c6dcbf731dbfe24cb02ba74b0bd19b9e4a7f5291
Iragemini <iragemini@gmail.com>
2021-08-11 01:42:48 +0300

    refactor: change names

---
+++ public/resolution/resolution.js
@@ -0,0 +1,32 @@
+const ttlDiv = document.querySelector('.ttl__input');
+const ttlCheckbox = document.getElementById('ttl');
+const ttlInput = document.getElementById('ttlInput');
+const deleteResolutionBtn = document.getElementById('deleteResolution');
+const doctorResolutionFound = document.getElementById('doctorResolutionFound');
+const patientName = document.getElementById('patientName');
+const showResolution = document.getElementById('showResolution');
+
+const url = 'http://localhost:3000';
+
+ttlCheckbox.onchange = () => {
+  ttlDiv.classList.toggle('ttl__input');
+  ttlInput.value = '';
+};
+
+showResolution.onclick = async () => {
+  const name = patientName.value.trim();
+  const response = await fetch(`${url}/resolution/${name}/show`);
+  doctorResolutionFound.value = await response.json();
+};
+
+deleteResolutionBtn.onclick = async () => {
+  const name = patientName.value.trim();
+  const response = await fetch(`${url}/resolution/${name}/delete`, {
+    method: 'DELETE',
+  });
+  if (response.ok) {
+    doctorResolutionFound.value = '';
+  } else {
+    doctorResolutionFound.value = await response.json();
+  }
+};
