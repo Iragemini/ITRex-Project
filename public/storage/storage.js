86b8d46f6d8a00c18e86f223c7cdc7d9b88f3956
Iragemini <iragemini@gmail.com>
2021-08-11 01:48:45 +0300

    refactor: delete files

--- public/storage/storage.js
+++
@@ -1,8 +0,0 @@
-const queue = [];
-const patients = [
-  { name: 'Vasia', resolutionObj: { resolution: 'bed rest', expire: '' } },
-];
-
-const storage = { queue, patients };
-
-export default storage;
