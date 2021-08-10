00bee4440732ddf16e9dcbef5164648dc9822751
Iragemini <iragemini@gmail.com>
2021-08-11 01:52:48 +0300

    refactor: update code after creating Queue module

--- public/queue/queue.js
+++ public/queue/queue.js
@@ -1,30 +1,13 @@
-import {
-  addPatientToQueue,
-  findResolution,
-  setCurrentPatient,
-} from '../src/handlers.js';
-
-const addPatient = document.getElementById('addPatient');
 const searchResolution = document.getElementById('searchResolution');
 const queueResolution = document.getElementById('queueResolution');
-const newPatient = document.getElementById('newPatient');
+const url = 'http://localhost:3000';
 
-addPatient.onclick = () => {
-  if (newPatient.value.trim() === '') {
-    newPatient.classList.add('is-invalid');
-    return;
-  }
-  newPatient.classList.remove('is-invalid');
-  const name = addPatientToQueue(newPatient.value.trim());
-  setCurrentPatient(name, 'queue');
-  newPatient.value = '';
-};
-
-searchResolution.addEventListener('keyup', (e) => {
+searchResolution.addEventListener('keyup', async (e) => {
   if (e.key === 'Enter') {
     e.preventDefault();
     const name = searchResolution.value.trim();
-    const resolution = findResolution(name);
+    const response = await fetch(`${url}/resolution/${name}/show`);
+    const resolution = await response.json();
     queueResolution.value = resolution;
   }
 });
