cb348d8ad4e822c2787267dd63b4d0fe444dec99
Iragemini <iragemini@gmail.com>
2021-08-11 17:58:43 +0300

    feat: add validation to queue

--- src/Queue/queue.routes.js
+++ src/Queue/queue.routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { add, remove, getCurrent } from './queue.controllers.js';
+import { validator } from '../validate/validate.js';
+import { nameSchema } from '../validate/schemas.js';
 
 const queueRouter = Router();
 
 queueRouter.get('/', getCurrent);
 queueRouter.post('/queue/add', add);
-queueRouter.delete('/queue/:name/delete', remove);
+queueRouter.delete(
+  '/queue/:name/delete',
+  validator(nameSchema, 'params'),
+  remove
+);
 
 export default queueRouter;
