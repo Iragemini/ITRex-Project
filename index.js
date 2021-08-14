8c8c336398940dce483b22e1eab4c1984d829c04
Iragemini <iragemini@gmail.com>
2021-08-14 18:56:15 +0300

    feat: change server settings

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 import express from 'express';
 import path from 'path';
-import methodOverride from 'method-override';
-import { config } from './config/default.json';
+import config from 'config';
 import queueRouter from './src/Queue/queue.routes.js';
 import resolutionRouter from './src/Resolution/resolution.routes.js';
 import { handle } from './src/Errors/errorHandler.js';
 
-const PORT = config.get('port') || 3000;
 const __dirname = path.resolve();
+const PORT = config.get('port') || 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './public');
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(express.json());
 
 app.use(queueRouter);
 app.use(resolutionRouter);
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Clinic',
+    currentPatient: '',
+    searchInput: '',
+    resolution: '',
+  });
+})
 app.use((req, res, next) => {
   res.status(404);
   if (req.accepts('json')) {
-    res.json({ error: 'Not found' });
+    res.json({ message: 'Route not found' });
     return;
   }
 });
