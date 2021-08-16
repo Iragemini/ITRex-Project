a3281f121152897f881aeb44c51a2e5d065609b6
Iragemini <iragemini@gmail.com>
2021-08-16 15:21:04 +0300

    feat: create makeRequest() method

--- public/http/Service.js
+++ public/http/Service.js
@@ -5,76 +5,69 @@ export default class Service {
   }
 
   errorHandler = (url, err) => {
-    let code = '';
-    let message = '';
-    if (err.code) {
-      code = err.code;
-    } else {
-      code = err.status;
-    }
-    if (err.message) {
-      message = err.message;
-    }
     const error = new Error();
+    const code = err.code ? err.code : err.status;
+    const message = err.message ? err.message : '';
     error.code = code;
     error.message = message;
     error.text = `Could not fetch ${url}, received ${code}`;
     throw error;
   };
 
+  makeRequest = async (url, options) => {
+    try {
+      const response = await fetch(`${this.base}${url}`, options);
+      if (!response.ok) {
+        this.errorHandler(`${this.base + url}`, await response.json());
+      }
+      const responseHeaders = response.headers.get('content-type');
+      const type = responseHeaders.split(';')[0];
+      let result = {};
+      if (type === 'application/json') {
+        result = await response.json();
+      }
+      return result;
+    } catch (error) {
+      this.errorHandler(url, error);
+    }
+  };
+
   /* GET */
   getResource = async (url) => {
-    const response = await fetch(`${this.base}${url}`, {
+    const options = {
       method: 'GET',
       headers: this.headers,
-    });
-    if (!response.ok) {
-      this.errorHandler(`${this.base + url}`, await response.json());
-    }
-    const result = await response.json();
-    return result;
+    };
+    return this.makeRequest(url, options);
   };
 
   /* POST */
   postResource = async (url, data = {}) => {
-    const response = await fetch(`${this.base}${url}`, {
+    const options = {
       method: 'POST',
       headers: this.headers,
       body: JSON.stringify(data),
-    });
-    if (!response.ok) {
-      this.errorHandler(`${this.base + url}`, await response.json());
-    }
-    const result = await response.json();
-    return result;
+    };
+    return this.makeRequest(url, options);
   };
 
   /* PATCH */
   patchResource = async (url, data = {}) => {
-    const response = await fetch(`${this.base}${url}`, {
+    const options = {
       method: 'PATCH',
       headers: this.headers,
       body: JSON.stringify(data),
-    });
-    if (!response.ok) {
-      this.errorHandler(`${this.base + url}`, await response.json());
-    }
-    const result = await response.json();
-    return result;
+    };
+    return this.makeRequest(url, options);
   };
 
   /* DELETE */
   deleteResource = async (url, data = {}) => {
-    const response = await fetch(`${this.base}${url}`, {
+    const options = {
       method: 'DELETE',
       headers: this.headers,
       body: JSON.stringify(data),
-    });
-
-    if (!response.ok) {
-      this.errorHandler(`${this.base + url}`, await response.json());
-    }
-    const result = await response.json();
-    return result;
+    };
+    return this.makeRequest(url, options);
   };
 }
