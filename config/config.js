1f23c221dfb8b2146036217c125f2f3ea6d27a8a
Iragemini <iragemini@gmail.com>
2021-08-18 19:06:43 +0300

    feat: add configs

--- config/config.js
+++ config/config.js
@@ -1,5 +1,14 @@
-export default {
-  server: {
-    port: process.env.PORT || 3000,
-  },
+const config = {};
+
+config.server = {
+  port: process.env.PORT || 3000,
 };
+
+/* type - parameter, described in ./storage.js */
+config.storage = {
+  type: 1,
+};
+
+config.ttl = '';
+
+export default config;
---
+++ config/storage.js
@@ -0,0 +1,12 @@
+export default {
+  memory: {
+    type: 0,
+  },
+  redis: {
+    type: 1,
+    client: {
+      port: 6379,
+      host: '127.0.0.1',
+    },
+  },
+};
