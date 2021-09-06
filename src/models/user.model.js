1bf439603670c2d225ea3a5ae3ada411e60a1c82
Iragemini <iragemini@gmail.com>
2021-09-07 02:52:55 +0300

    feat: update models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,25 +2,14 @@ export default (sequelize, Sequelize) => {
   const User = sequelize.define(
     'user',
     {
-      name: {
-        type: Sequelize.STRING,
-      },
-      gender: {
-        type: Sequelize.STRING,
-      },
-      birthday: {
-        type: Sequelize.DATE,
-      },
       email: {
         type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
       },
       password: {
         type: Sequelize.STRING,
-      },
-    },
-    {
-      defaultScope: {
-        attributes: { exclude: ['password'] },
+        allowNull: false,
       },
     },
   );
