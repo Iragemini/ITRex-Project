76accb3add268697850b699006bb859d0b72ce90
Iragemini <iragemini@gmail.com>
2021-09-27 19:39:15 +0300

    refactor: delete unused files and methods

--- src/utils/resolution.js
+++
@@ -1,8 +0,0 @@
-const isResolutionExpired = (expire) => {
-  if (!expire || Number.parseInt(expire, 10) >= Date.now()) {
-    return false;
-  }
-  return true;
-};
-
-export default isResolutionExpired;
