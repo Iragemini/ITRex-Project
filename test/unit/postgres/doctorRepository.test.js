48fe1a60c7788bddfff156abf353bb0bf5cc5afa
Iryna Karavaichyk <iryna.karavaichyk@iterxgroup.com>
2021-11-10 12:28:57 +0300

    test: update and add tests

--- test/unit/postgres/doctorRepository.test.js
+++ test/unit/postgres/doctorRepository.test.js
@@ -9,11 +9,13 @@ describe('Doctor repository tests', () => {
   const id = 1;
   const userId = 1;
   const title = 'specialization';
+  const specializationId = 1;
   const doctor = {
     id,
     user_id: userId,
     name,
     title,
+    specialization_id: specializationId,
   };
 
   const expectedDoctor = {
@@ -21,6 +23,7 @@ describe('Doctor repository tests', () => {
     userId,
     name,
     specialization: title,
+    specializationId,
   };
 
   afterEach(() => {
@@ -29,6 +32,7 @@ describe('Doctor repository tests', () => {
 
   describe('Get all doctors', () => {
     it('should return doctor list', async () => {
+      pool.query.resetHistory();
       pool.query.resolves({ rows: [doctor] });
 
       expect(await pgDoctor.getAll()).to.deep.equal([expectedDoctor]);
@@ -71,6 +75,92 @@ describe('Doctor repository tests', () => {
       pool.query.resolves({ rows: [] });
 
       expect(await pgDoctor.getByUserId(userId)).to.be.null;
+      expect(pool.query.calledOnce).to.be.true;
+    });
+  });
+
+  describe('Create doctor', () => {
+    it('should create doctor', async () => {
+      const newDoctor = {
+        id,
+        user_id: userId,
+        name,
+        specialization_id: specializationId,
+      };
+      pool.query.resolves({ rows: [newDoctor] });
+
+      expect(await pgDoctor.createDoctor({
+        userId,
+        specializationId,
+        name,
+      })).to.deep.equal(newDoctor);
+      expect(pool.query.calledOnce).to.be.true;
+    });
+  });
+
+  describe('Delete doctor', () => {
+    it('should delete doctor', async () => {
+      const deletedDoctor = {
+        id,
+        user_id: userId,
+        name,
+        specialization_id: specializationId,
+      };
+      pool.query.resolves({ rows: [deletedDoctor] });
+
+      expect(await pgDoctor.deleteDoctor(id)).to.deep.equal([deletedDoctor]);
+      expect(pool.query.calledOnce).to.be.true;
+    });
+  });
+
+  describe('Update doctor', () => {
+    it('should update doctor specialization', async () => {
+      const newSpecialization = 2;
+      const updatedDoctor = {
+        id,
+        user_id: userId,
+        name,
+        specialization_id: newSpecialization,
+      };
+      pool.query.resolves({ rows: [updatedDoctor] });
+
+      expect(await pgDoctor.updateDoctor({ specializationId: newSpecialization, name: null, id }))
+        .to.deep.equal(updatedDoctor);
+      expect(pool.query.calledOnce).to.be.true;
+    });
+
+    it('should update doctor name', async () => {
+      const newName = 'Doctor_new';
+      const updatedDoctor = {
+        id,
+        user_id: userId,
+        name: newName,
+        specialization_id: specializationId,
+      };
+      pool.query.resolves({ rows: [updatedDoctor] });
+
+      expect(await pgDoctor.updateDoctor({ specializationId: null, name: newName, id }))
+        .to.deep.equal(updatedDoctor);
+      expect(pool.query.calledOnce).to.be.true;
+    });
+
+    it('should update doctor name && specialization', async () => {
+      const newName = 'Doctor_new';
+      const newSpecialization = 2;
+      const updatedDoctor = {
+        id,
+        user_id: userId,
+        name: newName,
+        specialization_id: newSpecialization,
+      };
+      pool.query.resolves({ rows: [updatedDoctor] });
+
+      expect(await pgDoctor.updateDoctor({
+        specializationId: newSpecialization,
+        name: newName,
+        id,
+      }))
+        .to.deep.equal(updatedDoctor);
       expect(pool.query.calledOnce).to.be.true;
     });
   });
